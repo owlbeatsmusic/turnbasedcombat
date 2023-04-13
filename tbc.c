e2eaf3f18cfa7907414c58cafb61479827baf854
Allan Werner <swedengt06@gmail.com>
2023-04-13 22:10:11 +0200

    started work on adding and subtracting stats when doing a move

--- tbc.c
+++ tbc.c
@@ -84,20 +84,67 @@ int set_player_moves(Player* player, Move m1, Move m2, Move m3, Move m4, Move m5
 	return 0;
 }
 
-int add_to_stat(Player* player, Stat stat, int value) {
-
+// self and opnt can be reversed in the context of this function
+int add_to_stat(Player* self, Player* opnt, Stat stat, Move move, int value) {
+	switch(stat) {
+		case SPD:
+			self->SPD = value; 
+			break;
+		case HP :
+			if (value < 0 ) self->HP  += value * ((100.0-self->DEF)/100.0) * ((100.0+opnt->ATK)/110.0); // movedamage * defensemult. * attackmult.
+			if (value >= 0) self->HP  += value;
+			break;
+		case ATK: 
+			self->ATK += value; 
+			break;
+		case NRG: 
+			self->NRG += value; 
+			break;
+		case DEF: 
+			self->DEF += value; 
+			break;
+		case PRC: 
+			self->PRC += value; 
+			break;
+		case SNS: 
+			self->SNS += value; 
+			break;
+	}	
 	return 0;
 }
 
 int do_move(Player* self, Player* opnt, Move move) {
+	
+	// return if missed/dodged
 	if (move.always_hit == 0) {
-		float prob_of_hit = move.hit_prob * (self->PRC/105.0) - (BASE_DODGE_PROB) * ((100.0+opnt->SPD)/120.0);
-		for (int i = 0; i < 10; i++) {	
-			float random = (float)rand() / RAND_MAX;
-			if (prob_of_hit < random)printf("[  MOVE  ]: Missed %f < %f\n", prob_of_hit, random);
-			else printf("[  MOVE  ]: Hit %f < %f\n", prob_of_hit, random);
+		float prob_of_hit = move.hit_prob * (self->PRC/100.0) - (BASE_DODGE_PROB) * ((100.0+opnt->SPD)/120.0); // moveprob. * precisionprob - dodgeprob.
+		float random = (float)rand() / RAND_MAX;
+		if (prob_of_hit < random) { 
+			printf("[  MOVE  ]: Missed/Dodged %f < %f\n", prob_of_hit, random);
+			return 0;
 		}
+		else printf("[  MOVE  ]: Hit %f < %f\n", prob_of_hit, random);
 	}
+	
+	// do move
+	add_to_stat(self, opnt, SPD, move, move.self_SPD);
+	add_to_stat(self, opnt, HP , move, move.self_HP );
+	add_to_stat(self, opnt, ATK, move, move.self_ATK);
+	add_to_stat(self, opnt, NRG, move, move.self_NRG);
+	add_to_stat(self, opnt, DEF, move, move.self_DEF);
+	add_to_stat(self, opnt, PRC, move, move.self_PRC);
+	add_to_stat(self, opnt, SNS, move, move.self_SNS);
+
+	add_to_stat(opnt, self, SPD, move, move.opnt_SPD);
+	add_to_stat(opnt, self, HP , move, move.opnt_HP );
+	add_to_stat(opnt, self, ATK, move, move.opnt_ATK);
+	add_to_stat(opnt, self, NRG, move, move.opnt_NRG);
+	add_to_stat(opnt, self, DEF, move, move.opnt_DEF);
+	add_to_stat(opnt, self, PRC, move, move.opnt_PRC);
+	add_to_stat(opnt, self, SNS, move, move.opnt_SNS);	
+	
+	printf("[  MOVE  ]: all add_to_stats' complete \n");
+	
 	return 0;
 }
 
